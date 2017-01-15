4f121e340f32c5b66c7e5a6fc7b17ef3b3f755ca
Andres <cornejo.cruz.andres@gmail.com>
2017-01-15 12:34:24 -0600

    now you can see the size of devices in get_name_disk.sh

--- get_name_disk.sh
+++ get_name_disk.sh
@@ -5,5 +5,6 @@ echo ------------
 for n in $(ls /dev/disk/by-label/); do
 echo $n
 file /dev/disk/by-label/$n | grep -o -i "[a-z]*[0-9]*$"
+fdisk -l | grep -i "$(file /dev/disk/by-label/$n | grep -oi "[a-z]\{3\}[0-9]*$")" | grep -o "[^ ][0-9]*.[0-9]*[GM] "
 echo ------------
 done
--- see3x.sh
+++ see3x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-for link in $(cat /home/andres/Documents/hs-Documents/newText.txt); do
+for link in $(cat ~/hs-Documents/newText.txt); do
   google-chrome --incognito $link
 done
